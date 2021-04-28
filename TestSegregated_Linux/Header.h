b9e17b5b433abdcf9abf4ecfee95f182f4bc467a
Anonymous <example@foo.com>
2021-04-29 00:15:47 +1100

    Foobar0

--- TestSegregated_Linux/Header.h
+++ TestSegregated_Linux/Header.h
@@ -12,4 +12,20 @@ namespace Hoge
 		struct Impl;
 		Segregated::CSegregated<Impl, 24> impl;
 	};
+
+	class CMyInt
+	{
+	public:
+		int ComplexCalc(int a) const { return value * a; }
+	private:
+		int value = 1800;
+	};
+
+	class CFoobar0
+	{
+	public:
+		int Calc(int a)const { return iVal.ComplexCalc(a); }
+	private:
+		CMyInt iVal;
+	};
 }
--- TestSegregated_Linux/main.cpp
+++ TestSegregated_Linux/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <random>
+#include <array>
 
 #include "Header.h"
 
@@ -7,6 +9,18 @@ int main()
 	using namespace std;
 	using namespace Hoge;
 
-	CHoge hoge;
-	printf("%d\n", hoge.Func(23));
+	//CHoge hoge;
+	//printf("%d\n", hoge.Func(23));
+
+	mt19937 rnd;
+	constexpr size_t size = 100;
+	array<int, size> rnds{};
+	for (size_t i = 0; i < size; ++i)
+		rnds[i] = static_cast<int>(rnd() % 12);
+
+	for (size_t i = 0; i < size; ++i)
+	{
+		CFoobar0 foobar;
+		cout << foobar.Calc(rnds[i]) << endl;
+	}
 }
