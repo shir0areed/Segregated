75675a8ea9ac9c7adbb2d9f163251dafe179136d
Anonymous <example@foo.com>
2021-04-29 01:40:02 +1100

    スレッド分けたけど変わらん

--- TestSegregated_Linux/main.cpp
+++ TestSegregated_Linux/main.cpp
@@ -4,6 +4,7 @@
 #include <chrono>
 #include <functional>
 #include <algorithm>
+#include <thread>
 
 #include "Header.h"
 
@@ -11,7 +12,7 @@ using namespace std;
 using namespace std::chrono;
 
 template<typename T>
-void Measure(const vector<int>& rnds, int wage)
+void Measure(const vector<int>& rnds, int wage, int64_t* ret)
 {
 	int64_t sum = 0;
 	auto start = steady_clock::now();
@@ -23,8 +24,7 @@ void Measure(const vector<int>& rnds, int wage)
 	auto end = steady_clock::now();
 	auto period = end - start;
 	cout << sum / 10000 << " man yen / " << rnds.size() / 365 << " years" << endl;
-	cout << duration_cast<microseconds>(period).count() << " usec." << endl;
-	cout << endl;
+	*ret = duration_cast<microseconds>(period).count();
 }
 
 int main()
@@ -42,8 +42,18 @@ int main()
 
 	for (int i = 0; i < 3; ++i)
 	{
-		Measure<Hoge::CFoobar0>(rnds, wage);
-		Measure<Hoge::CFoobar1>(rnds, wage);
-		Measure<Hoge::CFoobar2>(rnds, wage);
+		int64_t measure0 = 0;
+		int64_t measure1 = 0;
+		int64_t measure2 = 0;
+		auto th0 = thread{ Measure<Hoge::CFoobar0> , rnds, wage, &measure0 };
+		auto th1 = thread{ Measure<Hoge::CFoobar1> , rnds, wage, &measure1 };
+		auto th2 = thread{ Measure<Hoge::CFoobar2> , rnds, wage, &measure2 };
+		th0.join();
+		th1.join();
+		th2.join();
+		cout << measure0 << " usec." << endl;
+		cout << measure1 << " usec." << endl;
+		cout << measure2 << " usec." << endl;
+		cout << endl;
 	}
 }
