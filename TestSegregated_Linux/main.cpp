537b524dc8847476d5df56157c199413d4d6592a
Anonymous <example@foo.com>
2021-04-29 01:03:40 +1100

    Measure改良

--- TestSegregated_Linux/main.cpp
+++ TestSegregated_Linux/main.cpp
@@ -7,40 +7,41 @@
 
 #include "Header.h"
 
+using namespace std;
+using namespace std::chrono;
+
 template<typename T>
-void Measure()
+void Measure(const vector<int>& rnds)
 {
-	using namespace std;
-	using namespace std::chrono;
-
-	mt19937 rnd;
-	constexpr size_t size = 10000000;
-	vector<int> rnds(size);
-	for (size_t i = 0; i < size; ++i)
-		rnds[i] = static_cast<int>(rnd() % 12);
-
 	int sum = 0;
 	auto start = steady_clock::now();
-	for (size_t i = 0; i < size; ++i)
+	for (size_t i = 0; i < rnds.size(); ++i)
 	{
 		T foobar{};
 		sum += foobar.Calc(rnds[i]);
 	}
 	auto end = steady_clock::now();
 	auto period = end - start;
-	cout << sum / 10000 << " man yen / " << size / 365 << " years" << endl;
+	cout << sum / 10000 << " man yen / " << rnds.size() / 365 << " years" << endl;
 	cout << duration_cast<microseconds>(period).count() << " usec." << endl;
 	cout << endl;
 }
 
 int main()
 {
-	using namespace std;
-
 	//CHoge hoge;
 	//printf("%d\n", hoge.Func(23));
 
-	Measure<Hoge::CFoobar0>();
-	Measure<Hoge::CFoobar1>();
-	Measure<Hoge::CFoobar2>();
+	mt19937 rnd;
+	constexpr size_t size = 10000000;
+	vector<int> rnds(size);
+	for (size_t i = 0; i < rnds.size(); ++i)
+		rnds[i] = static_cast<int>(rnd() % 12);
+
+	for (int i = 0; i < 3; ++i)
+	{
+		Measure<Hoge::CFoobar0>(rnds);
+		Measure<Hoge::CFoobar1>(rnds);
+		Measure<Hoge::CFoobar2>(rnds);
+	}
 }
